d0700fb699f1850f8dc0427d992cb9c9cae03011
AltraSol <chris.ulch@hotmail.com>
2022-09-06 16:45:54 -0600

    Add documentation

--- R/get_downloads_folder.R
+++ R/get_downloads_folder.R
@@ -1,3 +1,11 @@
+#' Get downloads folder
+#'
+#' Return the path to the downloads directory regardless of operating system.
+#'
+#' @return A character file path.
+#' @export
+#' @examples
+#' get_downloads_folder()
 get_downloads_folder <- function() {
   if (Sys.info()["sysname"] == "Windows")
     folder <- paste0("C:/Users/", Sys.info()["login"])

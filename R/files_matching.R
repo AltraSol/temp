aa04f57c19dbb716f4a940cc46e934aabaebaff3
AltraSol <chris.ulch@hotmail.com>
2022-09-05 19:51:32 -0600

    Include magrittr, update documentation

--- R/files_matching.R
+++ R/files_matching.R
@@ -1,3 +1,12 @@
+#' Search a directory for files
+#'
+#' todo
+#'
+#' @return An integer vectors or list of integer vectors.
+#' @export
+#' @examples
+#' files_matching(getwd(), "pdf")
+#' files_matching(getwd(), "xlsm", name_contains = "a")
 files_matching <- function(path, file_type, name_contains = NULL, ignore.case = T) {
   file_type <- gsub("[[:punct:]]", "", file_type)
   file_type <- paste0("*.", file_type, "$")
@@ -14,12 +23,12 @@ files_matching <- function(path, file_type, name_contains = NULL, ignore.case = 
       full.names = T
     )
   last_modified <- file.mtime(path)
-  matching_df <- tibble(name, path, last_modified)
+  matching_df <- dplyr::tibble(name, path, last_modified)
   matching_df <- matching_df %>%
-    arrange(desc(last_modified))
+    dplyr::arrange(desc(last_modified))
   if (!missing(name_contains)) {
     matching_df <- matching_df %>%
-      filter(
+      dplyr::filter(
         grepl(
           name_contains,
           name,

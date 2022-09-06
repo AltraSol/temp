a9fcd79274668e16afee956f8fd327b066cdef85
AltraSol <chris.ulch@hotmail.com>
2022-09-05 19:23:21 -0600

    Add documentation, update related

--- R/drop_na_cols.R
+++ R/drop_na_cols.R
@@ -1,3 +1,15 @@
+#' Drop NA columns
+#'
+#' Returns a data frame without columns containing only NA values.
+#'
+#' @return A data frame or tibble.
+#' @export
+#' @seealso [drop_na_rows()]
+#' @examples
+#' sample_data <- data.frame(head(iris, 5), NA_col = rep(NA, 5))
+#' sample_data
+#'
+#' drop_na_cols(sample_data)
 drop_na_cols <- function(df) {
   return(df[ ,colSums(is.na(df)) < nrow(df)])
 }
--- R/drop_na_rows.R
+++ R/drop_na_rows.R
@@ -1,3 +1,15 @@
+#' Drop NA rows
+#'
+#' Returns a data frame without rows containing only NA values.
+#'
+#' @return A data frame or tibble.
+#' @export
+#' @seealso [drop_na_cols()]
+#' @examples
+#' sample_data <- rbind(head(iris, 5), rep(NA, 5))
+#' sample_data
+#'
+#' drop_na_rows(sample_data)
 drop_na_rows <- function(df) {
   return(df[rowSums(is.na(df)) < ncol(df), ])
 }

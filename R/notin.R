bd6a34043cdefbf52425587e5bb678f9f1073f3c
AltraSol <chris.ulch@hotmail.com>
2022-09-06 17:29:05 -0600

    Add %notin%

--- R/notin.R
+++ R/notin.R
@@ -1,1 +1,12 @@
+#' Negate value matching
+#'
+#' An intuitive counterpart to `%in%` using `Negate()` to return a logical
+#' vector indicating if the left operand is \emph{not in} the right operand.
+#'
+#' @return A vector of the same length as x.
+#' @export
+#' @seealso [base::match()] [base::Negate()]
+#' @examples
+#' 1:10 %notin% c(1,3,5,9)
 `%notin%` <- Negate(`%in%`)
+

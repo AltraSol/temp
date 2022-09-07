7a55b0f4965b6f65b0845f1b216b7943596aa842
AltraSol <chris.ulch@hotmail.com>
2022-09-06 18:19:23 -0600

    Add @seealso

--- R/vary-package.R
+++ R/vary-package.R
@@ -1,3 +1,15 @@
+#' @seealso Structure Data
+#' @seealso * [which_rows()]
+#' @seealso * [flatten_page_list()]
+#' @seealso * [drop_na_rows()]
+#' @seealso * [drop_na_cols()]
+#' @seealso Locate Files
+#' @seealso * [get_downloads_folder()]
+#' @seealso * [files_matching()]
+#' @seealso Other _(possible future relocation)_
+#' @seealso * [clean_cols_in()]
+#' @seealso * [clean_cols_out()]
+#' @seealso * [`%notin%`]
 #' @keywords internal
 "_PACKAGE"
 

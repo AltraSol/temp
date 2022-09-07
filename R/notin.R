094489adbbc6cbcf5e03535cfc37461e8b3148a8
AltraSol <chris.ulch@hotmail.com>
2022-09-06 18:02:40 -0600

    Fix %notin%

--- R/notin.R
+++ R/notin.R
@@ -1,12 +1,12 @@
 #' Negate value matching
 #'
-#' An intuitive counterpart to `%in%` using `Negate()` to return a logical
+#' An intuitive counterpart to `%in%` which returns a logical
 #' vector indicating if the left operand is \emph{not in} the right operand.
-#' @param x Vector or NULL: the values to be matched.
-#' @param y Vector or NULL: the values to be matched against.
+#' @param abc Vector or NULL: the values to be matched.
+#' @param xyz Vector or NULL: the values to be matched against.
 #' @return A vector of the same length as x.
 #' @export
 #' @seealso [base::match()] [base::Negate()]
 #' @examples
 #' 1:10 %notin% c(1,3,5,9)
-`%notin%` <- function(x, y) base::Negate(base::match(x, y, nomatch = 0) > 0)
+`%notin%` <- function(abc, xyz) base::match(abc, xyz, nomatch = 0) == 0

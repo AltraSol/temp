14ead975c3efdc4c5b994295539ed0513c8de1d0
AltraSol <chris.ulch@hotmail.com>
2022-09-19 14:08:43 -0600

    Add parameter

--- R/drop_na_rows.R
+++ R/drop_na_rows.R
@@ -1,16 +1,26 @@
 #' Drop NA rows
 #'
-#' Returns a data frame without rows containing only NA values.
+#' Returns a tibble or data frame without rows containing only NA values. If
+#' `percent_incomplete` is supplied, any row which has a total percentage of NA
+#' values greater than or equal to `percent_incomplete` will be dropped.
 #'
 #' @param df A data frame or tibble.
+#' @param percent_incomplete A numeric value.
 #' @return A data frame or tibble.
 #' @export
 #' @seealso [drop_na_cols()]
 #' @examples
-#' sample_data <- rbind(head(iris, 5), rep(NA, 5))
+#' sample_data <-
+#'   tibble(rbind(
+#'     head(iris, 5),
+#'     rep(NA, 5),
+#'     c(1, rep(NA, 4)))
+#'   )
+#'
 #' sample_data
 #'
 #' drop_na_rows(sample_data)
-drop_na_rows <- function(df) {
-  return(df[rowSums(is.na(df)) < ncol(df), ])
+#' drop_na_rows(sample_data, percent_incomplete = 75)
+drop_na_rows <- function(df, percent_incomplete = 100) {
+  return(df[(1 - rowSums(is.na(df))/ncol(df))*100 > (100 - percent_incomplete), ])
 }

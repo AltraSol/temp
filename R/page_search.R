63eabe3a027ec59aded5b3e24adc88517e280703
AltraSol <chris.ulch@hotmail.com>
2022-10-30 09:34:56 -0600

    Prevent ubuntu pdf_text error

--- R/page_search.R
+++ R/page_search.R
@@ -23,7 +23,7 @@
 #' @seealso [vary::flatten_pages()]
 #' @export
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' # return the R manual folder path
 #' r_manual_dir <- paste0(R.home("doc"), .Platform$file.sep, "manual")
 #'
